920a81f363940d79dabca15b747e40cb7516bfb1
GetHandi <102155679+GetHandi@users.noreply.github.com>
2023-06-06 00:06:13 +0900

    done

--- t3.cpp
+++ t3.cpp
@@ -13,7 +13,7 @@ void PrintMatrix(double* ptr[], const unsigned char SIZE_R, const unsigned char 
 		for (int j = 0; j < SIZE_C; j++) {
 			cout << setw(9) << ptr[i][j] << " ";
 		}
-		cout << endl;
+		cout << endl << endl;
 	}
 	cout << endl;
 }
@@ -21,14 +21,18 @@ void PrintMatrix(double* ptr[], const unsigned char SIZE_R, const unsigned char 
 
 int main()
 {
+	double eps = 0.00001;
 	const unsigned char SIZE = 5;
 
-	double matrix[][SIZE + 1] = { {38.0, -8.0, 9.0, -4.0, 8.0, 43.0},
-								{-1.0, 21.0, -5.0, 0.0, -2.0, 26.0},
-								{-1.0, -7.0, 15.0, 1.0, -4.0, 12.0},
-								{6.0, 6.0, -3.0, 25.0, 1.0, 70.0},
-								{-8.0, 5.0, 6.0, 1.0, 28.0, 32.0} };
+	double matrix[][SIZE + 1] = { {38.0, -8.0, 9.0, -4.0, 8.0 },
+								{-1.0, 21.0, -5.0, 0.0, -2.0 },
+								{-1.0, -7.0, 15.0, 1.0, -4.0 },
+								{6.0, 6.0, -3.0, 25.0, 1.0 },
+								{-8.0, 5.0, 6.0, 1.0, 28.0 } };
+	double free_terms[SIZE] = { 43.0, 26.0, 12.0, 70.0, 32.0 };
 	double result[] = { 0.0, 0.0, 0.0, 0.0, 0.0 };
+	double result2[] = { 0.0, 0.0, 0.0, 0.0, 0.0 };
+	double temp[SIZE] = { 43.0, 26.0, 12.0, 70.0, 32.0 };
 
 	// создание массива указателей массива matrix
 	double* ptr[SIZE]{};
@@ -36,16 +40,74 @@ int main()
 		ptr[i] = matrix[i];
 
 	// вывод матрицы в консоль;
-	PrintMatrix(ptr, SIZE, SIZE + 1);
+	PrintMatrix(ptr, SIZE, SIZE);
 
-	double new[] ;
-	x01 = x02 = x03 = 0.0;
+	// вывод свободных членов в консоль;
+	cout << "Free Terms:\n";
+	for (int i = 0; i < SIZE; i++)
+	{
+		cout << free_terms[i] << '\t';
+	}
+	cout << endl << endl;
 
 
+///////////////////////////////////////////////////////////////////////////////////
+	// расчет новых значенй матрицы result;
+	int k = 0;
+	while (k != -1)
+	{
+		k += 1;
+		// вычисления;
+		for (int i = 0; i < SIZE; i++)
+		{
+			for (int j = SIZE - 1; j >= 0; j--)
+			{
+				if (i != j)
+				{
+					temp[i] -= matrix[i][j] * result[j];
+				}
+			}
+			temp[i] /= matrix[i][i];
+			result[i] = temp[i];
+		}
 
+		// вывод промежуточного результата в консоль;
+		cout << k+1 << ") " << "Interim result:\n";
+		for (int i = 0; i < SIZE; i++)
+		{
+			cout << "x" << i+1 << " = " << result[i] << endl;
+		}
+		cout << endl << endl;
 
+		// присваивание временной переменной снова значения матрицы свободных членов;
+		for (int i = 0; i < SIZE; i++)
+		{
+			temp[i] = free_terms[i];
+		}
 
+		// проверка на eps;
+		if (abs(result[k-1] - result2[k-1]) > eps)
+		{
+			for (int i = 0; i < SIZE; i++)
+			{
+				result2[i] = result[i];
+			}
+		}
+		// выход из wbrkf while и вывод результатов в консоль;
+		else {
+			k = -1;
+			cout << "Result:\n";
+
+			for (int i = 0; i < SIZE; i++)
+			{
+				cout << "x" << i+1 << " = " << result[i] << endl;
+			}
+		}
+	}
+///////////////////////////////////////////////////////////////////////////////////
 
 
 	return 0;
 }
+
+
